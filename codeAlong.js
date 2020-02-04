9e6a466b683a5ff1594e80d03e24f8c866a205a0
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-04 13:14:01 -0500

    Add files via upload

--- codeAlong.js
+++ codeAlong.js
@@ -1,13 +1,33 @@
 // Graded Code Along
+for (var i = 0; i < 1000; i++) {
+    if (i == 1 || i == 2) {
+        console.log(i);
+    } else if(i % 2 != 0){
+        // the number is not 1 or 2 and is odd
+        var J = 5;
+        while(j* <= i) {
+            if (i% == 0 || i%(J*2) == 0) {
+                return false
+                console.log(i);
+            }        }
+    }
+}
 
-var groceryList = new Array();
-myArray[0]="Peanut Butter";
-myArray[1]="Oreos";
-myArray[2]="Yogurt";
-myArray[3]="Catfood";
-myArray[4]="dog treats";
-myArray[5]="treats for the cats";
-myArray[6]="milk";
-myArray[7]="bacon"
-myArray[8]="eggs";
-myArray[9]="bread";
+
+
+
+
+
+
+
+// var groceryList = new Array();
+// myArray[0]="Peanut Butter";
+// myArray[1]="Oreos";
+// myArray[2]="Yogurt";
+// myArray[3]="Catfood";
+// myArray[4]="dog treats";
+// myArray[5]="treats for the cats";
+// myArray[6]="milk";
+// myArray[7]="bacon"
+// myArray[8]="eggs";
+// myArray[9]="bread";
