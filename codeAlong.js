dae662b19c98376354020fbfd6738158c93be997
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-04 11:13:51 -0500

    Add files via upload

---
+++ codeAlong.js
@@ -0,0 +1,13 @@
+// Graded Code Along
+
+var groceryList = new Array();
+myArray[0]="Peanut Butter";
+myArray[1]="Oreos";
+myArray[2]="Yogurt";
+myArray[3]="Catfood";
+myArray[4]="dog treats";
+myArray[5]="treats for the cats";
+myArray[6]="milk";
+myArray[7]="bacon"
+myArray[8]="eggs";
+myArray[9]="bread";
